92e6ffc2c013d4742178c89ed24f192f9ef2ec2e
Mat Hayward <mat@madebykind.com>
2023-01-25 23:02:18 +0000

    Fix vh

---
+++ src/vh.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});

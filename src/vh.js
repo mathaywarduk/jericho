5a3ccb1bbdf749f5b1c275a809a0c0164469be7d
Mat Hayward <mat@madebykind.com>
2023-01-25 23:05:34 +0000

    remove domcontentload

--- src/vh.js
+++ src/vh.js
@@ -1,9 +1,7 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 window.addEventListener('resize', () => {
-  let vh = window.innerHeight * 0.01;
+  vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
 });

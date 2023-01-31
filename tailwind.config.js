f454e40d594cb2423098e3d31e32aee6bb24262a
Mat Hayward <mat@madebykind.com>
2023-01-31 09:09:05 +0000

    Homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,14 @@ module.exports = {
   ],
   theme: {
     fontSize: {
-      sm: "0.8125rem", // 13px
-      base: "1rem", // 16px
-      lg: "1.25rem", // 20px
-      xl: "1.5rem", // 24px
+      sm: "1rem", // 16px
+      base: "1.125rem", // 18px
+      md: "1.375rem", // 22px
+      lg: "1.6875rem", // 27px
+      xl: "2rem", // 32px
+      "2xl": "3.125rem", // 50px
+      "3xl": "3.75rem", // 60px
+      "4xl": "5rem", // 80px
     },
     screens: {
       sm: "375px",
@@ -24,11 +28,17 @@ module.exports = {
     },
     extend: {
       fontFamily: {
+        serif: ["Roboto Slab", ...defaultTheme.fontFamily.serif],
         sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
       },
       maxWidth: {
+        48: "12rem",
         75: "18.75rem",
+        "container": "160rem", 
       },
+      height: {
+        "screen-plus": "110vh",
+      }
     },
   },
   variants: {
